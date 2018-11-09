543e676cd85365e517efb198494e6ab9e00f6334
agriell <galkin_dmitry@mail.ru>
2018-11-10 01:40:14 +0700

    Home work
    HTML forms
    task 3 ok

--- html-forms/feedback/js/formHandler.js
+++ html-forms/feedback/js/formHandler.js
@@ -1,10 +1,16 @@
 const contentform = document.getElementsByClassName('contentform')[0],
 	  inputFields = document.getElementsByTagName('input'),
-	  outputFields = document.getElementsByTagName('output');
+	  outputFields = document.getElementsByTagName('output'),
+	  buttons = document.getElementsByClassName('button-contact');
 
 
-function sendMessage() {
+function sendMessage(ev) {
+	if (buttons[0].disabled) return;
+	ev.preventDefault()
 	for (let field of inputFields) {
+		if (field.name == 'message') {
+			document.getElementById('message').value = field.innerText;
+		};
 		if (document.getElementById(field.name) != null) {
 			document.getElementById(field.name).value = field.value
 		};
@@ -13,12 +19,32 @@ function sendMessage() {
 	document.getElementsByTagName('main')[0].classList.toggle('hidden');
 }
 
-// function sendMessage() {
-// 	let testSumm = 0;
-// 	for (field of inputFields) {
-// 		if (field.value) {testSumm++}
-// 	}
-// 	if (testSumm == 11) {
+function fillFormTest() {
+	for (let field of inputFields) {
+		if (!field.value) return false;
+	}
+	if (document.getElementsByTagName('textarea')[0].value) {return true}
+	else return false
+}
 
-// 	}
-// }
+function testState() {
+	if (fillFormTest()) {
+		buttons[0].disabled = false
+	} else {buttons[0].disabled = true}
+}
+
+inputFields[5].onkeypress = function(event) {
+	// console.log(event)
+	if (event.key >= 0 & event.key <= 9) {return event}
+		else return false;
+}
+
+
+for (field of inputFields) {
+	field.addEventListener('input', testState)	
+}
+document.getElementsByTagName('textarea')[0].addEventListener('input', testState);
+
+for (button of buttons) {
+	button.addEventListener('click', sendMessage)
+}
