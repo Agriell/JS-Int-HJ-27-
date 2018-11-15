a631f0723961876082ac251be903f343f0daa605
agriell <galkin_dmitry@mail.ru>
2018-11-15 21:33:08 +0700

    home work
    принципы обработки собтий - все задания решены

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const 	addTCart = document.getElementsByClassName('add-to-cart'),
+		showMoreBtn = document.getElementsByClassName('show-more')[0];
+
+function toAddCartAssign() {
+	Array.from(addTCart)
+		.forEach((item) => {
+			item.addEventListener('click', addToCartCrutch);
+		});
+};
+
+function addToCartCrutch(event) {
+	let {dataset: item} = event.target;
+	addToCart(item);
+};
+
+showMoreBtn.addEventListener('click', toAddCartAssign);
+toAddCartAssign();
--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,17 @@
 'use strict';
 
+const	propNames = document.getElementsByTagName('th'),
+		tab = document.getElementsByTagName('table')[0];
+
 function handleTableClick(event) {
-  
+	event.stopPropagation();
+	let { target: item } = event;
+
+	item.dataset.dir ? item.dataset.dir *= -1 : item.setAttribute('data-dir', 1);
+	tab.setAttribute('data-sort-by', item.dataset.propName);
+	sortTable(tab.dataset.sortBy, item.dataset.dir);
 }
+
+Array.from(propNames).forEach((item) => {
+	item.addEventListener('click', handleTableClick)
+})
