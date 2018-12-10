3687d39e12d45858880011a1f173617ba0657621
dmitrygalkin <galkin.dmitry.a@gmail.com>
2018-12-10 23:39:50 +0700

    html-elem-collection
    Исправлены замечания в 2 и 3 задачах

--- media/photo-booth/js/script.js
+++ media/photo-booth/js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const app = document.querySelector('.app');
+const photoList = document.querySelector('.list'); 
+let video = document.createElement('video');
+video.autoplay = true;
+app.appendChild(video);
+
+navigator.mediaDevices
+  .getUserMedia({video: true, audio: false})
+  .then(stream => {
+  	console.log(window.URL.createObjectURL(stream))
+  	video.src = window.URL.createObjectURL(stream);
+  })
+  .catch(err => console.warn('Какие-то проблемы с доступом к камере.. (('));
+
